945747e1eb9eb46e581b86e772a17281e79a6413
Sindre Sørensen <>
2021-02-12 16:05:42 +0100

    First subtask (import and row count) done.

--- main.py
+++ main.py
@@ -20,15 +20,15 @@ def main():
     sc = SparkContext(conf=conf)
 
     badges_rdd = sc.textFile("{}/badges.csv".format(path_to_data))
-    badges_rdd = sc.textFile("{}/comments.csv".format(path_to_data))
-    badges_rdd = sc.textFile("{}/posts.csv".format(path_to_data))
-    badges_rdd = sc.textFile("{}/users.csv".format(path_to_data))
+    comments_rdd = sc.textFile("{}/comments.csv".format(path_to_data))
+    posts_rdd = sc.textFile("{}/posts.csv".format(path_to_data))
+    users_rdd = sc.textFile("{}/users.csv".format(path_to_data))
 
 
-    lineLengths = badges_rdd.map(lambda s: len(s))
-    totalLength = lineLengths.reduce(lambda a, b: a + b)
-    print(lineLengths)
-    print(totalLength)
+    print("There is {} rows in {}".format(badges_rdd.count(), "badges"))
+    print("There is {} rows in {}".format(comments_rdd.count(), "comments"))
+    print("There is {} rows in {}".format(posts_rdd.count(), "posts"))
+    print("There is {} rows in {}".format(users_rdd.count(), "users"))
 
 
 if __name__ == "__main__":
