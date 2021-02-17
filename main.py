404a26c93b84be69f429900b663a14f4bb920a18
Sindre Sørensen <>
2021-02-17 12:15:44 +0100

    Changed structure of task 2.1, and implemented RDDs mean function.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import argparse
 from pyspark import SparkContext, SparkConf
-from pyspark.sql import DataFrameReader, SparkSession
+import base64
 
 
 def parse_path():
@@ -13,14 +13,56 @@ def parse_path():
     else:
         return "data/"
 
+
 def print_number_of_rows(rdd, rdd_name):
     print("There is {} rows in {}".format(rdd.count(), rdd_name))
 
+'''
+def find_avg_length(rdd, column_number, decode=False, req=None):  # TODO: Mark decode to True as default
 
-def print_avg_post_length(rdd):
-    post_length = rdd.map(lambda line: len(line))
-    avg_length = post_length.reduce(lambda a, b: a+b)
-    print(avg_length/rdd.count())
+    if req is not None:
+        column_length = rdd.map(lambda line:
+                                len(base64.b64decode(line).split("\t")[column_number])
+                                if decode else
+                                len(line.split("\t")[column_number]))
+    else:
+        column_length = rdd.map(lambda line:
+                                len(base64.b64decode(line).split("\t")[column_number])
+                                if decode else
+                                len(line.split("\t")[column_number]))
+
+    total_length = column_length.reduce(lambda a, b: a + b)
+    avg_length = total_length / rdd.count()
+    return avg_length
+'''
+
+def task_1_5(badges_rdd, comments_rdd, posts_rdd, users_rdd):
+    print_number_of_rows(badges_rdd, "badges")
+    print_number_of_rows(comments_rdd, "comments")
+    print_number_of_rows(posts_rdd, "posts")
+    print_number_of_rows(users_rdd, "users")
+
+
+def task_2_1(comments_rdd, posts_rdd):
+    # comment_column_length = comments_rdd.map(
+    #    lambda line: len(base64.b64decode(line).split("\t")[2]))  # Comments text is located in column 2
+    comment_column_length = comments_rdd.map(
+        lambda line: len(line.split("\t")[2]))  # Comments text is located in column 2
+    print("The average length of comment text is {}".format(comment_column_length.mean()))
+
+    question_column_length = posts_rdd.map(
+        lambda line: len(line.split("\t")[5])
+        if line.split("\t")[1] == '1' else -1)  # Answer body is in column 2 and PostTypeId in column 5 (1->question)
+    question_column_length = question_column_length.filter(lambda x: x != -1)
+    print("The average length of question body is {}".format(question_column_length.mean()))
+
+    answer_column_length = posts_rdd.map(
+        lambda line: len(line.split("\t")[5])
+        if line.split("\t")[1] == '2' else -1)  # Answer body is in column 2 and PostTypeId in column 5 (2->answer)
+    answer_column_length = answer_column_length.filter(lambda x: x != -1)
+    print("The average length of answer body is {}".format(answer_column_length.mean()))
+
+
 
 
 def main():
@@ -46,16 +88,32 @@ def main():
     h = users_rdd.first()
     users_rdd = users_rdd.filter(lambda x: x != h)
 
-
     # Task 1.5
-    print_number_of_rows(badges_rdd, "badges")
-    print_number_of_rows(comments_rdd, "comments")
-    print_number_of_rows(posts_rdd, "posts")
-    print_number_of_rows(users_rdd, "users")
+    print("\nTask 1.5 output below:")
+    #task_1_5(badges_rdd, comments_rdd, posts_rdd, users_rdd)
+
+    # Task 2.1
+    print("\nTask 2.1 output below:")
+    #task_2_1(comments_rdd, posts_rdd)
+
+    # Task 2.2
+    print("\nTask 2.2 output below:")
+    #dates = posts_rdd.map(lambda line: line.split("\t")[2])  # Dates is in column 2
+    #print(dates.collect())
+    #dates.
 
 
-    #Task 2.1
-    #print_avg_post_length(posts_rdd)
+    '''
+    print("The average length of comment text is {}".format(
+        find_avg_length(comments_rdd, 2)))  # Comments text is located in column 2
+    print("The average length of question body is {}"
+          .format(find_avg_length(posts_rdd, 5, req={"check_columns_number": 1,
+                                                     "must_be": 1})))  # TODO: Add comment
+    print("The average length of answer body is {}"
+          .format(find_avg_length(posts_rdd, 5, req={"check_columns_number": 1,
+                                                     "must_be": 1})))  # TODO: Add comment
+    '''
+
 
 
 
