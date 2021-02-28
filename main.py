750195e74e9dfe64ff42113095c7fb300dff6f5f
Sindre Sørensen <>
2021-02-28 23:03:03 +0100

    Creating graph in task 3 from RDDs. Doing SQL query to show weigh DESC.

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from graphframes import *
 from pyspark.sql import SparkSession
 from pyspark.sql import SQLContext
 
+
 # NB! Using pyspark 3.0.1
 
 # TODO: Write that parsing from csv to "pretty" rdd could happen once
@@ -66,6 +67,7 @@ def task_2_2(posts_rdd, users_rdd):
     user_of_oldest_date_tuple = users_parsed.filter(lambda user: int(user[0]) == int(oldest_date_tuple[0]))
     print("Username of oldest post is {}".format(user_of_oldest_date_tuple.first()[3]))
 
+
 def task_2_3(posts_rdd):
     questions = posts_rdd.map(lambda line: line.split("\t")).filter(
         lambda line: line[1] == '1' and line[6] != '-1' and line[6] != 'NULL')  # Filter out non-questions
@@ -75,8 +77,12 @@ def task_2_3(posts_rdd):
         lambda line: line[1] == '2' and line[1] != '-1' and line[6] != 'NULL')  # Filter out non-answers
     answers_usernames = answers.map(lambda x: (x[6], 1))
     answers_username_max = answers_usernames.reduceByKey(add).max(key=lambda x: x[1])
-    print("The username that has posted the most questions is {} with {} posts".format(questions_username_max[0], questions_username_max[1]))
-    print("The username that has posted the most answers is {} with {} posts".format(answers_username_max[0], answers_username_max[1]))
+
+    # TODO: Join with users to get DISPLAY NAME!
+    print("The username that has posted the most questions is {} with {} posts".format(questions_username_max[0],
+                                                                                       questions_username_max[1]))
+    print("The username that has posted the most answers is {} with {} posts".format(answers_username_max[0],
+                                                                                     answers_username_max[1]))
 
 
 def task_2_4(badges_rdd):
@@ -120,79 +126,100 @@ def main():
     conf = SparkConf().setAppName("TDT4305 project").setMaster("local")
     sc = SparkContext(conf=conf)
 
+
     #  Dependency and import for part 3
     sc.addPyFile("graphframes-0.8.1-spark3.0-s_2.12.jar")
     import graphframes
 
-    # Task 1
-    badges_rdd = sc.textFile("{}/badges.csv".format(path_to_data))
-    h_badges = badges_rdd.first()
-    badges_rdd = badges_rdd.filter(lambda x: x != h_badges)
-
-    comments_rdd = sc.textFile("{}/comments.csv".format(path_to_data))
-    h_comments = comments_rdd.first()
-    comments_rdd = comments_rdd.filter(lambda x: x != h_comments)
-
+    # Task 1.1
     posts_rdd = sc.textFile("{}/posts.csv".format(path_to_data))
     h_posts = posts_rdd.first()
     posts_rdd = posts_rdd.filter(lambda x: x != h_posts)
 
+    # Task 1.2
+    comments_rdd = sc.textFile("{}/comments.csv".format(path_to_data))
+    h_comments = comments_rdd.first()
+    comments_rdd = comments_rdd.filter(lambda x: x != h_comments)
+
+    # Task 1.3
     users_rdd = sc.textFile("{}/users.csv".format(path_to_data))
     h_users = users_rdd.first()
     users_rdd = users_rdd.filter(lambda x: x != h_users)
 
+    # Task 1.4
+    badges_rdd = sc.textFile("{}/badges.csv".format(path_to_data))
+    h_badges = badges_rdd.first()
+    badges_rdd = badges_rdd.filter(lambda x: x != h_badges)
+
     # Task 1.5
     print("\nTask 1.5 output below:")
-    #task_1_5(badges_rdd, comments_rdd, posts_rdd, users_rdd)
+    # task_1_5(badges_rdd, comments_rdd, posts_rdd, users_rdd)
 
     # Task 2.1
     print("\nTask 2.1 output below:")
-    #task_2_1(comments_rdd, posts_rdd)
+    # task_2_1(comments_rdd, posts_rdd)
 
     # Task 2.2
     print("\nTask 2.2 output below:")
-    #task_2_2(posts_rdd, users_rdd)
+    # task_2_2(posts_rdd, users_rdd)
 
     # Task 2.3
     print("\nTask 2.3 output below:")
-    #task_2_3(posts_rdd)
+    # task_2_3(posts_rdd)
 
     # Task 2.4
     print("\nTask 2.4 output below:")
-    #task_2_4(badges_rdd)
+    # task_2_4(badges_rdd)
 
     # Task 2.5
     print("\nTask 2.5 output below:")
-    #task_2_5(users_rdd)
+    # task_2_5(users_rdd)
 
     # Task 2.6
     print("\nTask 2.6 output below:")
-    #task_2_6(comments_rdd)
+    # task_2_6(comments_rdd)
 
+    # Task 3.1
+    # Creating the RDD in which later can be converted to DFs, then used for a graph
     sqlContext = SQLContext(sc)
 
-    users_with_display_name = users_rdd.map(lambda line: (line.split("\t")[0], line.split("\t")[3]))
+    users_with_display_name = users_rdd \
+        .map(lambda line: (line.split("\t")[0], line.split("\t")[3]))  # K->user id, V->DisplayName
+    posts_with_user_id = posts_rdd \
+        .map(lambda line: (line.split("\t")[0], line.split("\t")[6]))  # K->post id, V->OwnerUserId (user id)
+    comments_with_user_id = comments_rdd \
+        .map(lambda line: (line.split("\t")[0], line.split("\t")[4]))  # K->post id (from posts), V->UserID(user id)
 
-    # Create a Vertex DataFrame with unique ID column "id"
-    v = sqlContext.createDataFrame([
-        ("a", "Alice", 34),
-        ("b", "Bob", 36),
-        ("c", "Charlie", 30),
-    ], ["id", "name", "age"])
+    posts_and_comments_joined = posts_with_user_id \
+        .join(comments_with_user_id)  # Performs join on post id, V->(user id of comment, user id of post)
 
-    # Create an Edge DataFrame with "src" and "dst" columns
-    e = sqlContext.createDataFrame([
-        ("a", "b", "friend"),
-        ("b", "c", "follow"),
-        ("c", "b", "follow"),
-    ], ["src", "dst", "relationship"])
+    all_edges = posts_and_comments_joined \
+        .map(lambda line: (line[1], 1))  # K->(user id of comment, user id of post), V->1
+    edges_with_count = all_edges.reduceByKey(add)  # K->(user id of comment, user id of post), V->number of edges
+    edges_with_count = edges_with_count\
+        .map(lambda line: (line[0][0], line[0][1], line[1])) #  (user id of comment, user id of post, number of edges)
 
+    # Task 3.2
+    # Converting the RDDs to DFs of edges and vertices, then used to create a graph
 
-    # Create a GraphFrame
-    g = graphframes.GraphFrame(v, e)
+    # Converting RDDs to DFs
+    vertices = sqlContext.createDataFrame(users_with_display_name).toDF("id", "displayName")
+    edges = sqlContext.createDataFrame(edges_with_count).toDF("src", "dst", "w")
+
+    edges.registerTempTable("edges")
+
+    res = sqlContext.sql("SELECT src, SUM(w) FROM edges GROUP BY src ORDER BY SUM(w) DESC")
+    res.show()
+
+    '''
+    # Create the graph using GraphFrame
+    graph = graphframes.GraphFrame(vertices, edges)
 
     # Query: Get in-degree of each vertex.
-    g.inDegrees.show()
+    #graph.inDegrees.show()
+    top = graph.pageRank(resetProbability=0.1, maxIter=1)
+    top.outDegrees.show()
+    '''
     '''
     # Query: Count the number of "follow" connections in the graph.
     g.edges.filter("relationship = 'follow'").count()
@@ -201,8 +228,6 @@ def main():
     results = g.pageRank(resetProbability=0.01, maxIter=20)
     results.vertices.select("id", "pagerank").show()
     '''
-
-
 
 
 if __name__ == "__main__":
